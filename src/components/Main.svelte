a5c638d0aa29f244e792255c4c76c992faff438d
Andra Steele <andrasteele@gmail.com>
2023-12-17 15:20:04 -0800

    Added routing, sections

---
+++ src/components/Main.svelte
@@ -0,0 +1,37 @@
+<script>
+
+</script>
+
+<main class='flex flex-col flex-1 p-4'>
+  <section id='introPage' class='grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14'>
+    <div class='flex flex-col Lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10'>
+      <h2 class='font-semibold text-4xl sm:text-5xl md:text-6xl'>
+        Hi! I'm <span class='poppins text-violet-400'>Andra</span> Steele<br/>Software
+        <span class='poppins text-violet-400'>Engineer</span>
+      </h2>
+      <p class='text-base sm:text-lg md:text-xl'>
+          My <span class='text-violet-400'>favorite tehcnologies and skills</span> include Node.js, React & React Native, API development, writing documentation, Express.js, PostgreSQL and system design.
+      </p>
+      <a class='blueShadow mx-auto lg:mr-auto text-base s:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-slate text-white-950 cursor-pointer'
+      href='https://www.linkedin.com/in/andra-steele/'>
+        <div class='absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200'/>
+        <h4 class='relative z-9'>Get in touch &rarr;</h4>
+      </a>
+    </div>
+    <div class='relative shadow-2xl grid place-items-center'>
+      <img src={'images/profile.png'} alt="Profile image" class='object-cover z-[2] max-h-[70vh] opacity-2'/>
+    </div>
+
+  </section>
+  <section id='projects' class='py-20 lg:py-32 flex flex-col gap-24'>
+    <div class='flex flex-col gap-2 text-center'>
+      <h6 class='text-lg sm:text-xl md:text-2xl'>
+        A few of my creative endeavors.
+      </h6>
+      <h3 class='font-semibold text-3xl sm:text-4xl md:text-5xl'>
+        Curious to <span class='poppins text-violet-400'>see</span> my work?
+      </h3>
+    </div>
+
+  </section>
+</main>
