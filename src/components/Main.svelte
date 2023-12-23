b50d0fa03f288a07470c681ee7b77f9774253c83
Andra Steele <andrasteele@gmail.com>
2023-12-22 21:11:34 -0800

    Added about

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,4 +1,18 @@
 <script>
+  import Step from './Step.svelte';
+
+  let steps = [
+    {name: 'Sneaky Snacker', icon: 'fa-solid fa-cookie-bite'},
+    {name: 'My Mixtape', icon: 'fa-solid fa-music'},
+    {name: 'Stencil Shaper', icon: 'fa-solid fa-pencil'}
+  ]
+
+  let benefits = [
+    {name: 'Excellent communicator', description: 'My approach to software engineering is informed by the better part of a decade of experience working within the hospitality sector as a chef. I have developed a skillset that prioritizes intentional communication, accountability, and team work as best practice.'},
+    {name: 'Documentation enthusiast', description: 'One of my strong suits has always been writing. When it comes to development, I enjoy being able to document a piece of software\'s use-- identifying patterns, use cases, edge cases, and writing accessible user guides.'},
+    {name: 'Empathetic collaborator', description:''},
+    {name: 'Driven developer', description:''}
+  ]
 
 </script>
 
@@ -19,7 +33,7 @@
       </a>
     </div>
     <div class='relative shadow-2xl grid place-items-center'>
-      <img src={'images/profile.png'} alt="Profile image" class='object-cover z-[2] max-h-[70vh] opacity-2'/>
+      <!-- <img src={'images/profile.png'} alt="Profile image" class='object-cover z-[2] max-h-[70vh] opacity-2'/> -->
     </div>
 
   </section>
@@ -32,6 +46,38 @@
         Curious to <span class='poppins text-violet-400'>see</span> my work?
       </h3>
     </div>
+    <div class='grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10'>
+      <Step step={steps[0]}>
+        <p>Sneaky snacker is an adventure game telling the story of a girl and her pup as they embark on a journey to acquire the elusive cookie jar. This full-stack game was created with
+          <strong class='text-violet-400'>React, Socket.io, Inquirer.js, Material-UI, and OpenAI's ChatGPT API integration.</strong>
+        </p>
+      </Step>
+      <Step step={steps[1]}>
+        <p>My Mixtape is a web application that creates personalized Spotify playlists based on a user's current vibe and saves to their Spotify profile. This project was created with
+          <strong class='text-violet-400'>React, MongoDB, Auth0's Spotify integration, and the Spotify API's Authorization Code Flow.</strong>
+        </p>
+      </Step>
+      <Step step={steps[2]}>
+        <p>Stencil Shaper is a cross-platform mobile application that is designed to help tattoo artists create custom stencils for their clients based on custom dimensions. StencilShaper provides an intuitive and user-friendly interface to customize and export stencils easily. This application was created with
+          <strong class='text-violet-400'>React Native & Expo Go.</strong>
+        </p>
+      </Step>
+     
+    </div>
 
+  </section>
+  <section id='about' class='py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative'>
+    <div class='flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4'>
+      <h6 class='text-lg sm:text-xl md:text-2xl'>Want to know more?</h6>
+      <h3 class='font-semibold text-3xl sm:text-4xl md:text-5xl'>A bit <span class='poppins text-violet-400'>about</span> me.</h3>
+    </div>
+    <p class='mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl'>I am</p>
+    <div class='flex flex-col gap-20 w-full mx-auto max-w-[800px]'>
+      {#each benefits as benefit}
+      <!-- <div class='flex gap-6 sm:gap-8'> -->
+       
+      <!-- </div> -->
+      {/each}
+    </div>
   </section>
 </main>
