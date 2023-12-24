0eefa12a348d23458ef0576136360342bd80791d
Andra Steele <andrasteele@gmail.com>
2023-12-24 14:04:33 -0800

    Added About, project paths.

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -2,16 +2,19 @@
   import Step from './Step.svelte';
 
   let steps = [
-    {name: 'Sneaky Snacker', icon: 'fa-solid fa-cookie-bite'},
-    {name: 'My Mixtape', icon: 'fa-solid fa-music'},
+    {name: 'Sneaky Snacker', 
+    icon: 'fa-solid fa-cookie-bite',
+    href: 'https://sneaky-snacker.netlify.app/'},
+    {name: 'My Mixtape', 
+    icon: 'fa-solid fa-music',
+    href: 'https://mymixtape.netlify.app/'},
     {name: 'Stencil Shaper', icon: 'fa-solid fa-pencil'}
   ]
 
   let benefits = [
-    {name: 'Excellent communicator', description: 'My approach to software engineering is informed by the better part of a decade of experience working within the hospitality sector as a chef. I have developed a skillset that prioritizes intentional communication, accountability, and team work as best practice.'},
-    {name: 'Documentation enthusiast', description: 'One of my strong suits has always been writing. When it comes to development, I enjoy being able to document a piece of software\'s use-- identifying patterns, use cases, edge cases, and writing accessible user guides.'},
-    {name: 'Empathetic collaborator', description:''},
-    {name: 'Driven developer', description:''}
+    {name: 'An excellent communicator', description: 'My approach to software engineering is informed by the better part of a decade of experience working within the hospitality sector as a chef. I have developed a skillset that prioritizes intentional communication, accountability, and team work as best practice.'},
+    {name: 'A documentation enthusiast', description: 'One of my strong suits has always been writing. When it comes to development, I enjoy being able to document a piece of software\'s use-- identifying patterns, use cases, edge cases, and writing accessible user guides.'},
+    {name: 'A driven developer', description:'A facet I enjoy about development that reminds me of cooking is the ever-changing industry. There will always be new techniques, skills, languages, and toolkits to learn. The pursuit of continuous growth and learning is a lifelong passion I hope to never lose. I strive to bring fresh eyes and skills to any project.'}
   ]
 
 </script>
@@ -68,16 +71,25 @@
   </section>
   <section id='about' class='py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative'>
     <div class='flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4'>
-      <h6 class='text-lg sm:text-xl md:text-2xl'>Want to know more?</h6>
-      <h3 class='font-semibold text-3xl sm:text-4xl md:text-5xl'>A bit <span class='poppins text-violet-400'>about</span> me.</h3>
+      <!-- <h6 class='text-lg sm:text-xl md:text-2xl'>Want to know more?</h6> -->
+      <h3 class='font-semibold text-3xl sm:text-4xl md:text-5xl'>About<span class='poppins text-violet-400'></span> me.</h3>
     </div>
     <p class='mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl'>I am</p>
     <div class='flex flex-col gap-20 w-full mx-auto max-w-[800px]'>
       {#each benefits as benefit}
-      <!-- <div class='flex gap-6 sm:gap-8'> -->
-       
-      <!-- </div> -->
-      {/each}
+      <div class="flex gap-6 sm:gap-8">
+        <p
+            class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+        >
+        </p>
+        <div class="flex flex-col gap-6 sm:gap-8">
+            <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                {benefit.name}
+            </h3>
+            <p>{benefit.description}</p>
+        </div>
     </div>
+{/each}
+</div>
   </section>
 </main>
